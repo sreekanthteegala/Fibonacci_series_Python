5a416ec3176fc8d42a94b328a5ac60a7a8f54bc6
Sreekanth Teegala <ryker034@gmail.com>
2022-12-04 18:56:30 +0530

    Initial commit

---
+++ Project.py
@@ -0,0 +1,13 @@
+import math
+n=list(map(int,input("Enter the numbers separated by ',': ").split(",")))
+def isPerfectSquare(x):
+    s = int(math.sqrt(x))
+    return s*s == x
+def isFibonacci(b):
+    return isPerfectSquare(5*b*b + 4) or isPerfectSquare(5*b*b - 4)
+for i in (n):
+    if (isFibonacci(i) == True):
+        print(i, "is valid")
+    else:
+        print(i, "is not valid")
+
